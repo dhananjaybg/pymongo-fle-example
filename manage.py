f91f373f29320dff640239686741b4ec9a1dcfa5
Ethan Steininger <esteininger21@gmail.com>
2020-04-13 17:36:04 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 import os
 from pymongo import MongoClient
 from pymongo.encryption_options import AutoEncryptionOpts
-from pymongo.encryption import ClientEncryption
+from pymongo.encryption import Algorithm, ClientEncryption
 import base64
 from bson.codec_options import CodecOptions
 from bson.binary import STANDARD, UUID
 from pprint import pprint
+from collections import OrderedDict
 
 
 class BuildEncryption:
@@ -16,28 +17,27 @@ class BuildEncryption:
         self.key_vault_namespace = f"{self.db}.{self.collection}"
         self.master_file_path = master_file_path
 
-    def create_master_key(self):
+    def create_kms_provider(self):
         file_bytes = os.urandom(96)
+
         with open(self.master_file_path, "wb") as f:
             f.write(file_bytes)
 
-
-    def create_kms_provider(self):
         with open(self.master_file_path, "rb") as f:
             local_master_key = f.read()
 
-        kms_providers = {
-            "local": {
-            "key": local_master_key # local_master_key variable from the previous step
+            kms_providers = {
+                "local": {
+                    "key": local_master_key  # local_master_key variable from the previous step
+                }
             }
-        }
 
-        return kms_providers
+            return kms_providers
 
-
-    def create_data_encryption_key(self):
+    def create_data_encryption_key(self, kms_providers):
         client_encryption = ClientEncryption(
-            self.create_kms_provider(), # pass in the kms_providers variable from the previous step
+            # pass in the kms_providers variable from the previous step
+            kms_providers,
             self.key_vault_namespace,
             self.client,
             CodecOptions(uuid_representation=STANDARD)
@@ -48,19 +48,70 @@ class BuildEncryption:
         base_64_data_key_id = base64.b64encode(data_key_id)
         return data_key_id
 
-
     def test(self, data_key_id):
         key_vault = self.client[self.db][self.collection]
         # Pass in the data_key_id created in previous section
         key = key_vault.find_one({"_id": data_key_id})
         pprint(key)
 
+    def create_schema(self, data_key_id):
+        db = self.client.test
+        collection = 'persons'
+        user_schema = {
+            "ssn": {
+                "encrypt": {
+                    "keyId": [data_key_id],
+                    "algorithm": Algorithm.AEAD_AES_256_CBC_HMAC_SHA_512_Deterministic,
+                    "bsonType": "string"
+                }
+            }
+        }
+
+        validator = {
+            '$jsonSchema': {
+                'bsonType': 'object',
+                'properties': user_schema
+            }
+        }
+
+        query = [('collMod', collection), ('validator', validator)]
+        db.command(OrderedDict(query))
+        return user_schema
+
+
+class EncryptedConnection:
+    def __init__(self, kms, schema):
+        key_vault_namespace = "encryption.__keyVault"
+
+        extra_options = {}
+
+        fle_opts = AutoEncryptionOpts(
+            kms,
+            key_vault_namespace,
+            schema_map=schema,
+            **extra_options
+        )
+        self.client = MongoClient(
+            "mongodb://localhost:27017", auto_encryption_opts=fle_opts)
+        self.collection = self.client['test']['persons']
+
+    def insert(self, name, ssn):
+        user = {
+            'name': name,
+            'ssn': ssn
+        }
+        self.collection.insert_one(user)
+
 
 if __name__ == '__main__':
-    encrypt = BuildEncryption(master_file_path="key.txt")
+    path = 'key.txt'
+    encrypt = BuildEncryption(master_file_path=path)
 
-    encrypt.create_master_key()
-
-    data_key_id = encrypt.create_data_encryption_key()
+    kms = encrypt.create_kms_provider()
+    data_key_id = encrypt.create_data_encryption_key(kms_providers=kms)
 
     encrypt.test(data_key_id=data_key_id)
+    schema = encrypt.create_schema(data_key_id=data_key_id)
+
+    client = EncryptedConnection(kms=kms, schema=schema)
+    client.insert(name="John", ssn="123")
